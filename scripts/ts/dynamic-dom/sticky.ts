3d8485bf25eb92c33608fe4d7c636da74b2b4c04
LukeStock22 <l.d.stockbridge@wustl.edu>
2024-09-09 14:58:59 -0500

    struggling w sticky header

--- scripts/ts/dynamic-dom/sticky.ts
+++ scripts/ts/dynamic-dom/sticky.ts
@@ -1,3 +1,22 @@
 export function setupSticky() {
     // TODO: Copy and paste your sticky code in here!!
+    var header = document.getElementById("main-menu-container");
+    
+    //Ensure header exists
+    if (!header) {
+        console.warn("Header element not found!");
+        return;
+    }
+   
+    //get offset
+    const sticky = header.offsetTop;
+   
+    // Add scroll event listener
+    window.onscroll = function() {
+        if (window.scrollY > sticky) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    };
  }
